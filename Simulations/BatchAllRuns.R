f115983684689dc118ddd5d0812e1de21ea65e80
Brian OMeara <omeara.brian@gmail.com>
2018-07-24 16:05:21 -0400

    removing old, old sims

--- Simulations/BatchAllRuns.R
+++
@@ -1,7 +0,0 @@
-#devtools::install_github("bomeara/BMhyb")
-
-n.cores=parallel::detectCores()
-for (core.index in sequence(n.cores)) {
-	system("nohup R CMD BATCH BatchRuns.R > /dev/null &")
-	Sys.sleep(5)
-}
